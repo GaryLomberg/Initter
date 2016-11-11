d4ac6e9749b3894d0b7cd2181354f0185829af21
Gary Lomberg <g.lmbrg@gmail.com>
2016-11-11 20:45:43 +0200

    Add TODO list

---
+++ initter.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+# Initer - A script to initialise new projects
+
+# TODO: Accept Project name and create new folder
+
+# TODO: Create new file within folder
+
+# TODO: Create README.md
+
+# TODO: Run git init
+
+# TODO: Push project to Github
