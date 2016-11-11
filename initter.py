bf7da01e0021deea1ee2d9487ad1ed0fd0635d22
Gary Lomberg <g.lmbrg@gmail.com>
2016-11-11 21:58:19 +0200

    Complete up to commiting to git

--- initter.py
+++ initter.py
@@ -1,12 +1,40 @@
 #! /usr/bin/python3
 # Initer - A script to initialise new projects
 
+import sys, os, subprocess
+
 # TODO: Accept Project name and create new folder
+
+projectName = sys.argv[1]
+
+if not os.path.exists(projectName):
+    os.makedirs(projectName)
+else:
+	print('That directory name already exists')
+	sys.exit()
+
+# TODO: Change directory into new folder
+
+os.chdir(projectName)
 
 # TODO: Create new file within folder
 
+projectFile = open( '{}.py'.format(projectName), 'w+')
+projectFile.write("#! /usr/bin/python3 \n")
+projectFile.write("# {} - \n".format(projectName))
+projectFile.close()
+
 # TODO: Create README.md
 
+readme = open('README.md', 'w+')
+readme.write("Project name: {} \n".format(projectName))
+readme.close()
+
 # TODO: Run git init
+
+subprocess.Popen(['git', 'init'], stdout=subprocess.PIPE)
+subprocess.Popen(['git', 'add', '.'], stdout=subprocess.PIPE)
+subprocess.Popen(['git', 'commit', '-m','"Initial commit"'], stdout=subprocess.PIPE)
+
 
 # TODO: Push project to Github
